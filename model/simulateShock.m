845cd716980e8b8140a05dc0d3a444a814c89b3b
Saketh Aleti <saketh10@gmail.com>
2017-04-28 23:10:03 -0400

    Add shocked prices to simulateShock

--- model/simulateShock.m
+++ model/simulateShock.m
@@ -1,5 +1,6 @@
-function [ price_t, quantity_t, price_eql,quantity_eql  ] = ...
-   simulateShock(price, quantity, elas_D, elas_S, alpha_shock, iterations)
+function [ price_t, quantity_t, price_shock, quantity_shock, price_eql, ...
+    quantity_eql ] = simulateShock(price, quantity, elas_D, elas_S,     ...
+    alpha_shock, iterations)
 
 % SIMULATESHOCK determines the change in price and quantity over time given
 % a shock to a system
@@ -17,8 +18,10 @@ function [ price_t, quantity_t, price_eql,quantity_eql  ] = ...
 % OUTPUT:
 %   price_t              (matrix) price vectors over time
 %   quantity_t           (matrix) quantity vectors over time
-%   price_eql            (vector) equillibrium prices
-%   quantity_eql         (vector) equillibrium quantities
+%   price_shock          (vector) prices after shock (partial equilibrium)
+%   quantity_shock       (vector) quantities after shock
+%   price_eql            (vector) general equillibrium prices
+%   quantity_eql         (vector) general equillibrium quantities
 
 %% Determine Coefficients
 
@@ -30,11 +33,8 @@ function [ price_t, quantity_t, price_eql,quantity_eql  ] = ...
 
 output = calculateShockCrossEffects( price, quantity, ...
                             alpha_d, beta_d, alpha_s, beta_s, alpha_shock);
-                       
-price_eql = output(:,1);
-quantity_eql = output(:,2);
-alpha_s2 = alpha_s + alpha_shock;
 
+alpha_s2 = alpha_s + alpha_shock;
 
 %% Simulate price and quantity movement over time
 
@@ -51,13 +51,27 @@ for t = 2:iterations
     
     % calculate price and quantity at time t
     for i = 1:size(price_t,1)
-        temp            = linsolve(beta_s-diag(diag(beta_d)), ...
+        
+        ptemp           = linsolve(beta_s - diag(diag(beta_d)), ...
                                    alpha_d + sum(temp_j(i,:)) - alpha_s2);
-        price_t(i,t)    = temp(i);
-        quantity_t(i,t) = alpha_s2 + beta_s*price_t(i,t);
+        price_t(i,t)    = ptemp(i);
+        
+        qtemp           = alpha_s2 + beta_s*ptemp;
+        quantity_t(i,t) = qtemp(i);
+        
     end
     
 end
+
+%% Find precise price and quantity in equilibrium
+
+price_shock    = linsolve((beta_s - diag(diag(beta_d))), alpha_d + ...
+                    (beta_d*price - diag(diag(beta_d))*price) - alpha_s2);
+quantity_shock = alpha_s2 + beta_s*price_shock
+
+price_eql = linsolve(beta_s - beta_d, alpha_d - alpha_s2);
+quantity_eql = alpha_s2 + beta_s*price_eql;
+
 
 end
 

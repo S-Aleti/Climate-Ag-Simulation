df58b531e4653daab6c4ee346c092ae5896e0e2e
Saketh Aleti <saketh10@gmail.com>
2017-05-17 20:46:41 -0400

    Introduce scripts to get KDI and MC results
    - MC => Monte Carlo trials

--- main_KDI.m
+++ main_KDI.m
@@ -1,7 +1,7 @@
 % table for price changes
 % compute rebound effect which is the fall in gasoline supply
 % price change for various shocks
-% gasoline 95 gco2/mj of emissions, 1.3*10^2 mJ/gallon
+% gasoline 95 gco2/mj of emissions, 131.76 mJ/gallon
 
 close all
 
@@ -9,10 +9,10 @@ close all
 %% Parameters
 
 % commodities to analyze
-commodities = {'natural gas', 'electricity', 'gasoline'};
+commodities = {'gasoline', 'electricity', 'natural gas'};
 
 % define percent changes in quantity (>1 for positive supply shocks)
-percent_change = [0.00:0.01:0.20] + 1;
+percent_change = [0.05:0.01:0.30] + 1;
 
 % iterations for the market simulation
 iterations = 15;
@@ -20,61 +20,46 @@ iterations = 15;
 
 %% Import Data
 
-filename  = 'elasticity_data.xlsx';
-elas_data = collectElasticityData(filename, 1, 2, 3, 4, 5, 6);
+disp(['Importing elasticity and production data...']);
 
-filename  = 'production_data.xlsx';
-pq_data   = collectPriceQuantityData(filename, 'main', 1, 2, 3, 8, 4, 6);
+filename_elasticities  = 'KDI_elasticity_data.xlsx';
+filename_production  = 'KDI_production_data.xlsx';
 
-[price,quantity, elas_D,elas_S] = ...
-    organizeData(commodities,elas_data,pq_data);
+[price,quantity, elas_D,elas_S] = importKDIData( commodities, ...
+    filename_elasticities, filename_production );
 
 
 %% Determine shock values 
 
+disp(['Calculating shock effects...']);
+
 [ alpha_d, beta_d, alpha_s, beta_s ] = calculateCoefficients( ...
                                          elas_D, elas_S, price, quantity);
 
-% calculate 
-alpha_shocks = quantity * percent_change - ...
-                repmat(beta_s*price + alpha_s, 1, size(percent_change,2));
-      
-% no shock to commodity 1 or 2
-alpha_shocks(1,:) = 0;
-alpha_shocks(2,:) = 0;
+% % calculate alpha shock values based on percent change
+% alpha_shocks = quantity * percent_change - ...
+%                 repmat(beta_s*price + alpha_s, 1, size(percent_change,2));
+%       
+% % no shock to commodity 1 or 2
+% alpha_shocks(1,:) = 0;
+% alpha_shocks(2,:) = 0;
 
+shock_data = collectCommodityShocks('KDI_supply_shocks', 'electricity', 2, 3);
+alpha_shocks = zeros(size(commodities, 2), size(shock_data, 1));
+alpha_shocks(2,:) = [shock_data{:,2}];
 
 %% Simluate shocks
 
-% initialize vars to 0
-price_eqls     = zeros(size(commodities,2),size(percent_change,2));
-quantity_eqls  = price_change;
-
-% plots the supply and demand graphs
 plot_results = false;
 
-for shock_num = 1:size(percent_change,2)
-    
-    % specific supply shock
-    alpha_shock = alpha_shocks(:,shock_num);
-    alpha_s2 = alpha_s + alpha_shock;
-    
-    % results of each shock
-    [ price_t, quantity_t, price_shock, quantity_shock, price_eql,      ...
-        quantity_eql ] = simulateShock(price, quantity, elas_D, elas_S, ...
-                        alpha_shock, iterations, plot_results);
-                    
-    price_eqls(:,shock_num)     = price_eql;
-    quantity_eqls(:,shock_num)  = quantity_eql;  
-    
-    
-end
+[price_eqls, quantity_eqls ] = runSimulation( price, quantity, elas_D, ...
+                        elas_S, alpha_shocks, iterations, plot_results);
 
 
 %% Results
 
 % number of supply shocks simulated
-n = size(percent_change,2);
+n = size(alpha_shocks,2);
 
 % Price changes
 price_change = price_eqls - repmat(price, 1, n);
@@ -90,12 +75,16 @@ quantity_rebound = quantity_eqls - quantity_after_shock;
 percent_quantity_rebound = quantity_rebound ./ quantity_after_shock;
 
 % Decrease in CO2
-CO2_reduction_grams = -quantity_rebound(3,:)*95*1.3*10^2;
-CO2_reduction_megatonnes = CO2_reduction_grams*10^(-6-6);
+CO2_reduction_grams = -quantity_rebound(1,:)*95*131.76;
+CO2_reduction_megatonnes = CO2_reduction_grams*10^(-6);
         
+% Formatted Data for xlsx
+formatted_data = [percent_price_change', percent_quantity_change', ...
+                    percent_quantity_rebound', CO2_reduction_megatonnes'];
 
 %% Plot Results
 
+pause;
 close all;
 
 % Price changes
@@ -154,12 +143,8 @@ set(plot_prices(1),'DisplayName', 'Decrease in CO2');
 
 lgd = legend('show');
 xlabel('Supply Shock (%)');
-ylabel('Megatonnes');
+ylabel('CO2 (megatonnes)');
 title('CO2 Reduction');
 grid on;
 
 
-
-
-        
-        
---
+++ main_MC.m
@@ -0,0 +1,194 @@
+% table for price changes
+% compute rebound effect which is the fall in gasoline supply
+% price change for various shocks
+% gasoline 95 gco2/mj of emissions, 131.76 mJ/gallon
+
+close all
+
+
+%% Parameters
+
+% commodities to analyze
+commodities = {'gasoline', 'electricity', 'natural gas'};
+
+% define percent changes in quantity (>1 for positive supply shocks)
+percent_change = [0.05:0.10:0.25] + 1;
+
+% iterations for the market simulation
+iterations = 15;
+
+
+%% Import Data
+
+disp(['Importing elasticity and production data...']);
+
+filename_elasticities  = 'KDI_elasticity_data.xlsx';
+filename_production  = 'KDI_production_data.xlsx';
+
+[price,quantity, elas_D,elas_S] = importKDIData( commodities, ...
+    filename_elasticities, filename_production );
+
+
+%% Determine shock values 
+
+disp(['Calculating shock effects...']);
+
+[ alpha_d, beta_d, alpha_s, beta_s ] = calculateCoefficients( ...
+                                         elas_D, elas_S, price, quantity);
+
+% calculate alpha shock values based on percent change
+alpha_shocks = quantity * percent_change - ...
+                repmat(beta_s*price + alpha_s, 1, size(percent_change,2));
+      
+% no shock to commodity 1 or 2
+alpha_shocks(2,:) = 0;
+alpha_shocks(3,:) = 0;
+
+% shock_data = collectCommodityShocks('KDI_supply_shocks', 'electricity', 2, 3);
+% alpha_shocks = zeros(size(commodities, 2), size(shock_data, 1));
+% alpha_shocks(2,:) = [shock_data{:,2}];
+
+%% Monte Carlo
+
+% params
+sigma_D = 0.03;
+sigma_S = 0.02;
+trials = 1000;
+plot_results = false; 
+
+% preallocate arrays
+n = size(alpha_shocks,2);
+price_change = zeros(size(commodities,2), n, trials);
+percent_price_change = price_change;
+quantity_change = price_change;
+percent_quantity_change = price_change;
+quantity_after_shock = price_change;
+quantity_rebound = price_change;
+percent_quantity_rebound = price_change;
+CO2_reduction_grams = price_change(1,:,:);
+CO2_reduction_megatonnes = CO2_reduction_grams;
+
+tic
+% open waitbar
+h = waitbar(0,'Running MC Sim');
+data_size = trials;
+
+for trial = 1:trials
+
+    % add RV to elasticities
+    [elas_D2, elas_S2] = randomizeElasticities(elas_D, elas_S, sigma_D, ...
+                                                    sigma_S);
+    
+    %% Simluate shocks
+    
+    [price_eqls, quantity_eqls ] = runSimulation( price, quantity, ...
+                 elas_D2, elas_S, alpha_shocks, iterations, plot_results);
+             
+    % update wait bar
+    if (mod(trial,200)==0) waitbar(trial/data_size,h); end
+
+    %% Results
+	
+    % Price changes
+    price_change(:,:,trial) = price_eqls - repmat(price, 1, n);
+    percent_price_change(:,:,trial) = price_change(:,:,trial) ./ ...
+                                        repmat(price, 1, n);
+   
+    % Quantity changes
+    quantity_change(:,:,trial) = quantity_eqls - repmat(quantity, 1, n);
+    percent_quantity_change(:,:,trial) = quantity_change(:,:,trial) ./ ...
+                                            repmat(quantity, 1, n);
+    quantity_after_shock(:,:,trial) = repmat(quantity,1,n) + alpha_shocks;
+    
+    % Rebound effect
+    quantity_rebound(:,:,trial) = quantity_eqls - ...
+                                    quantity_after_shock(:,:,trial);
+    percent_quantity_rebound(:,:,trial) = quantity_rebound(:,:,trial) ./...
+                                           quantity_after_shock(:,:,trial);
+    
+    % Decrease in CO2
+    CO2_reduction_grams(:,:,trial) = -quantity_rebound(1,:,trial)*95*131.76;
+    CO2_reduction_megatonnes(:,:,trial) = CO2_reduction_grams(:,:,trial)*10^(-6);
+    
+    % Formatted Data for xlsx
+    %formatted_data = [percent_price_change', percent_quantity_change', ...
+    %                    percent_quantity_rebound', CO2_reduction_megatonnes'];
+                    
+end
+
+close(h);
+toc
+
+%% Plot results for commodity j
+
+close all;
+
+% PARAMETERS
+j = 1; % commodity index from "commodities" var
+bins = 20;
+
+
+% Price changes
+fig = figure;
+
+for i = 1:length(percent_change)
+    h(i) = histogram(reshape(percent_price_change(j,i,:),1,trials),bins);
+    set(h(i),'DisplayName',[num2str(percent_change(i)-1), '% shock']);
+    hold on;
+end
+
+lgd = legend('show');
+xlabel('Price change (%)');
+ylabel(['Frequency', ' (max: ', num2str(trials), ')']);
+title(['Change in price of ', commodities{j}, ' after supply shocks']);
+grid on;
+
+
+% Quantity changes
+fig = figure;
+
+for i = 1:length(percent_change)
+    h(i) = histogram(reshape(percent_quantity_change(j,i,:),1,trials),bins);
+    set(h(i),'DisplayName',[num2str(percent_change(i)-1), '% shock']);
+    hold on;
+end
+
+lgd = legend('show');
+xlabel('Quantity change (%)');
+ylabel(['Frequency', ' (max: ', num2str(trials), ')']);
+title(['Change in quantity of ', commodities{j}, ' after supply shocks']);
+grid on;
+
+
+% Rebound effect
+fig = figure;
+
+for i = 1:length(percent_change)
+    h(i) = histogram(reshape(percent_quantity_rebound(j,i,:),1,trials),bins);
+    set(h(i),'DisplayName',[num2str(percent_change(i)-1), '% shock']);
+    hold on;
+end
+
+
+lgd = legend('show');
+xlabel('Rebound effect (%)');
+ylabel(['Frequency', ' (max: ', num2str(trials), ')']);
+title(['Rebound effect of ', commodities{j}, ' after supply shocks']);
+grid on;
+
+
+% CO2 reduction
+fig = figure;
+
+for i = 1:length(percent_change)
+    h(i) = histogram(reshape(CO2_reduction_megatonnes(1,i,:),1,trials),bins);
+    set(h(i),'DisplayName',[num2str(percent_change(i)-1), '% shock']);
+    hold on;
+end
+
+lgd = legend('show');
+xlabel('CO2 Reduction (megatonnes)');
+ylabel(['Frequency', ' (max: ', num2str(trials), ')']);
+title(['CO2 reduction after supply shocks']);
+grid on;
+

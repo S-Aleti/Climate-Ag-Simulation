297aa95506675d4ca0d7250c8fb34690735915d5
S_A <16127909+SA3291@users.noreply.github.com>
2017-11-26 22:08:36 -0500

    Update model comparison
    - Includes average quantity change
    - Now exports results

--- main/modelComparison.m
+++ main/modelComparison.m
@@ -13,9 +13,9 @@
 trials = 10;           % monte carlo trials
 format_results = false; % set to true to get formatted results
 % elasticities
-elas_S_corn_soybean = -0.076/10;
-elas_S_soybean_corn = -0.13/10;
-elas_D_corn_soybean = 0.123/10;
+elas_S_corn_soybean = -0.076;
+elas_S_soybean_corn = -0.13;
+elas_D_corn_soybean = 0.123;
 
 % pre-alloc output for each multimarket trial
 running_output = zeros(24*10, 7, trials); 
@@ -29,7 +29,7 @@ tic
 for trial = 1:trials
     
     % generate random elasticity
-    elas_D_soybean_corn = random(pd)/10;
+    elas_D_soybean_corn = random(pd);
 
     [ results, ~, ~, ~ ]  = analyzeShocksCross(epq_data,                ...
         cf_data, format_results, elas_S_corn_soybean,                   ...
@@ -49,6 +49,17 @@ multimarket_result = mean(running_output, 3);
     analyzeShocksCross(epq_data, cf_data, 0,0,0,0,0);
 
 
+%% Export results to csv
+
+% multimarket results
+writetable(formatOutput(multimarket_result, labels ), ...
+    'results/csv/results_multimarket.csv')
+
+% partial eq results
+writetable(formatOutput(partial_eq_result, labels ), ...
+    'results/csv/results_partialeq.csv')
+
+
 %% Compare changes in price, cons surplus, and prod surplus of soybean
 
 countrycrops = {{'China', 'soybean'}, {'India', 'soybean'},             ...
@@ -56,7 +67,7 @@ countrycrops = {{'China', 'soybean'}, {'India', 'soybean'},             ...
 [ indexes ] = findCountryCropIndex( countrycrops, labels );
 
 
-%%% Changes in price
+%%Percent Changes in price
 
 % multimarket price changes
 price_change_mm = [];
@@ -78,7 +89,7 @@ subplot(2,1,1);
 plot(0:9, price_change_pe)
 legend({'China', 'India', 'US'})
 title('Price Change of Soybean (Partial Eq)')
-xlabel('Year'); ylabel('% Change');
+xlabel('Year'); ylabel('Percent Change');
 axis([0 9 -1 1])
 grid('on')
 
@@ -87,12 +98,12 @@ subplot(2,1,2);
 plot(0:9, price_change_mm)
 legend({'China', 'India', 'US'})
 title('Price Change of Soybean (Multi-Market)')
-xlabel('Year'); ylabel('% Change');
+xlabel('Year'); ylabel('Percent Change');
 axis([0 9 -1 1])
 grid('on')
 
 
-%%% Changes in consumer surplus
+%%Percent Changes in consumer surplus
 
 % multimarket consumer surplus changes
 cs_change_mm = [];
@@ -114,7 +125,7 @@ subplot(2,1,1);
 plot(0:9, cs_change_pe)
 legend({'China', 'India', 'US'})
 title('Consumer Surplus Change of Soybean (Partial Eq)')
-xlabel('Year'); ylabel('% Change');
+xlabel('Year'); ylabel('Percent Change');
 grid('on')
 
 % multimarket subplot
@@ -122,11 +133,11 @@ subplot(2,1,2);
 plot(0:9, cs_change_mm)
 legend({'China', 'India', 'US'})
 title('Consumer Surplus Change of Soybean (Multi-Market)')
-xlabel('Year'); ylabel('% Change');
+xlabel('Year'); ylabel('Percent Change');
 grid('on')
 
 
-%%% Changes in producer surplus
+%%Percent Changes in producer surplus
 
 % multimarket producer surplus changes
 ps_change_mm = [];
@@ -148,7 +159,7 @@ subplot(2,1,1);
 plot(0:9, ps_change_pe)
 legend({'China', 'India', 'US'})
 title('Producer Surplus Change of Soybean (Partial Eq)')
-xlabel('Year'); ylabel('% Change');
+xlabel('Year'); ylabel('Percent Change');
 grid('on')
 
 % multimarket subplot
@@ -156,7 +167,7 @@ subplot(2,1,2);
 plot(0:9, ps_change_mm)
 legend({'China', 'India', 'US'})
 title('Producer Surplus Change of Soybean (Multi-Market)')
-xlabel('Year'); ylabel('% Change');
+xlabel('Year'); ylabel('Percent Change');
 grid('on')
 
 
@@ -167,7 +178,7 @@ countrycrops = {{'China', 'corn'}, {'India', 'corn'},                   ...
 [ indexes ] = findCountryCropIndex( countrycrops, labels );
 
 
-%%% Changes in price
+%%Percent Changes in price
 
 % multimarket price changes
 price_change_mm = [];
@@ -189,7 +200,7 @@ subplot(2,1,1);
 plot(0:9, price_change_pe)
 legend({'China', 'India', 'US'})
 title('Price Change of Corn (Partial Eq)')
-xlabel('Year'); ylabel('% Change');
+xlabel('Year'); ylabel('Percent Change');
 axis([0 9 -0.5 1])
 grid('on')
 
@@ -198,12 +209,12 @@ subplot(2,1,2);
 plot(0:9, price_change_mm)
 legend({'China', 'India', 'US'})
 title('Price Change of Corn (Multi-Market)')
-xlabel('Year'); ylabel('% Change');
+xlabel('Year'); ylabel('Percent Change');
 axis([0 9 -0.5 1])
 grid('on')
 
 
-%%% Changes in consumer surplus
+%%Percent Changes in consumer surplus
 
 % multimarket consumer surplus  changes
 cs_change_mm = [];
@@ -225,7 +236,7 @@ subplot(2,1,1);
 plot(0:9, cs_change_pe)
 legend({'China', 'India', 'US'})
 title('Consumer Surplus Change of Corn (Partial Eq)')
-xlabel('Year'); ylabel('% Change');
+xlabel('Year'); ylabel('Percent Change');
 axis([0 9 -0.5 1])
 grid('on')
 
@@ -234,12 +245,12 @@ subplot(2,1,2);
 plot(0:9, cs_change_mm)
 legend({'China', 'India', 'US'})
 title('Consumer Surplus Change of Corn (Multi-Market)')
-xlabel('Year'); ylabel('% Change');
+xlabel('Year'); ylabel('Percent Change');
 axis([0 9 -0.5 1])
 grid('on')
 
 
-%%% Changes in producer surplus
+%%Percent Changes in producer surplus
 
 % multimarket producer surplus changes
 ps_change_mm = [];
@@ -261,7 +272,7 @@ subplot(2,1,1);
 plot(0:9, ps_change_pe)
 legend({'China', 'India', 'US'})
 title('Producer Surplus Change of Corn (Partial Eq)')
-xlabel('Year'); ylabel('% Change');
+xlabel('Year'); ylabel('Percent Change');
 axis([0 9 -0.5 1])
 grid('on')
 
@@ -270,7 +281,7 @@ subplot(2,1,2);
 plot(0:9, ps_change_mm)
 legend({'China', 'India', 'US'})
 title('Producer Surplus Change of Corn (Multi-Market)')
-xlabel('Year'); ylabel('% Change');
+xlabel('Year'); ylabel('Percent Change');
 axis([0 9 -0.5 1])
 grid('on')
 
@@ -311,6 +322,7 @@ title('Change in Quantity of Chinese Soybean')
 y_ticks = round(get(gca,'ytick'), 3);
 set(gca, 'yticklabel', [num2str(y_ticks'*100)                           ...
     repmat('%', length(y_ticks), 1)])
+xlabel('Year'); ylabel('Percent Change');
 grid('on')
 
 %%% Indian Soybean
@@ -342,6 +354,7 @@ title('Change in Quantity of Indian Soybean')
 y_ticks = round(get(gca,'ytick'), 3);
 set(gca, 'yticklabel', [num2str(y_ticks'*100)                           ...
     repmat('%', length(y_ticks), 1)])
+xlabel('Year'); ylabel('Percent Change');
 grid('on')
 
 %%% US Soybean
@@ -373,6 +386,7 @@ title('Change in Quantity of US Soybean')
 y_ticks = round(get(gca,'ytick'), 3);
 set(gca, 'yticklabel', [num2str(y_ticks'*100)                           ...
     repmat('%', length(y_ticks), 1)])
+xlabel('Year'); ylabel('Percent Change');
 grid('on')
 
 %%% Chinese Corn
@@ -404,6 +418,7 @@ title('Change in Quantity of Chinese Corn')
 y_ticks = round(get(gca,'ytick'), 3);
 set(gca, 'yticklabel', [num2str(y_ticks'*100)                           ...
     repmat('%', length(y_ticks), 1)])
+xlabel('Year'); ylabel('Percent Change');
 grid('on')
 
 %%% Indian Corn
@@ -435,11 +450,12 @@ title('Change in Quantity of Indian Corn')
 y_ticks = round(get(gca,'ytick'), 3);
 set(gca, 'yticklabel', [num2str(y_ticks'*100)                           ...
     repmat('%', length(y_ticks), 1)])
+xlabel('Year'); ylabel('Percent Change');
 grid('on')
 
 %%% US Corn
 
-[ indexes ] = findCountryCropIndex(countrycrops(3), labels);
+[ indexes ] = findCountryCropIndex(countrycrops(6), labels);
 
 figure; hold on;
 plot(0:9, counterfactual_percent_shocks(indexes, :))
@@ -466,4 +482,83 @@ title('Change in Quantity of US Corn')
 y_ticks = round(get(gca,'ytick'), 3);
 set(gca, 'yticklabel', [num2str(y_ticks'*100)                           ...
     repmat('%', length(y_ticks), 1)])
+xlabel('Year'); ylabel('Percent Change');
 grid('on')
+
+
+%% Average Quantity Model Comparison
+
+%%% Soybean
+
+[ indexes ] = findCountryCropIndex(countrycrops(1:3), labels);
+
+% counterfactual quantity changes
+avg_q_change_cf = mean(counterfactual_percent_shocks(indexes, :), 2)';
+
+% multimarket quantity changes
+avg_q_change_mm = zeros(1, 3);
+
+% partial eq quantity changes
+avg_q_change_pe = zeros(1, 3);
+
+for i = 0:9 % for each year
+    avg_q_change_mm = avg_q_change_mm + ...
+        multimarket_result(indexes+24*i,2)'/10;
+    avg_q_change_pe = avg_q_change_pe + ...
+        partial_eq_result(indexes+24*i,2)'/10;
+end
+
+figure; 
+
+plot_labels = {};
+
+for i = 1:3
+    plot_labels{i} = countrycrops{i}{1};
+end
+
+bar(categorical(plot_labels), ...
+    [avg_q_change_cf; avg_q_change_mm; avg_q_change_pe]')
+y_ticks = round(get(gca,'ytick'), 3);
+set(gca, 'yticklabel', [num2str(y_ticks'*100)                           ...
+    repmat('%', length(y_ticks), 1)])
+legend({'Counterfactual', 'Multi-Market', 'Partial Equillibrium'})
+xlabel('Year'); ylabel('Average Percent Change');
+title('Average % Change in Quantity of Soybean')
+
+
+%%% Corn
+
+[ indexes ] = findCountryCropIndex(countrycrops(4:6), labels);
+
+% counterfactual quantity changes
+avg_q_change_cf = mean(counterfactual_percent_shocks(indexes, :), 2)';
+
+% multimarket quantity changes
+avg_q_change_mm = zeros(1, 3);
+
+% partial eq quantity changes
+avg_q_change_pe = zeros(1, 3);
+
+for i = 0:9 % for each year
+    avg_q_change_mm = avg_q_change_mm + ...
+        multimarket_result(indexes+24*i,2)'/10;
+    avg_q_change_pe = avg_q_change_pe + ...
+        partial_eq_result(indexes+24*i,2)'/10;
+end
+
+figure; 
+
+plot_labels = {};
+
+for i = 4:6
+    plot_labels{i-3} = countrycrops{i}{1};
+end
+
+bar(categorical(plot_labels), ...
+    [avg_q_change_cf; avg_q_change_mm; avg_q_change_pe]')
+y_ticks = round(get(gca,'ytick'), 3);
+set(gca, 'yticklabel', [num2str(y_ticks'*100)                           ...
+    repmat('%', length(y_ticks), 1)])
+legend({'Counterfactual', 'Multi-Market', 'Partial Equillibrium'})
+xlabel('Year'); ylabel('Average Percent Change');
+title('Average % Change in Quantity of Corn')

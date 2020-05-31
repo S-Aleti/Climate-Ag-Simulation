88ef3f48b8775b351e89916412ba6f1fc8d68b33
S_A <16127909+SA3291@users.noreply.github.com>
2020-05-31 13:37:25 -0400

    Update results
    - use new set of shocks

--- main/mainScenarios.m
+++ main/mainScenarios.m
@@ -5,24 +5,25 @@
 %% Params
 
 % scenario data
-scenarios_xlsx_file = '/crop_data/xlsx_data/CLM5_Country_Shock_Ave.xlsx';
+scenarios_xlsx_file = '/crop_data/xlsx_data/B_C_US_shocks_c_s.xlsx';
 
 % results file loc
-results_file = 'results/csv/results_CLM5_mm.csv';
+results_file = 'results/csv/results_05302020_pe.csv';
 
 % elasticities: use CGE elasticity 
 elas_S_corn_soybean = 0;
 elas_S_soybean_corn = 0;
-elas_D_corn_soybean = 0.808;
-elas_D_soybean_corn = 0.808;
+elas_D_corn_soybean = 0%.808;
+elas_D_soybean_corn = 0%.808;
 
 
 %% Collect Data
 
 % change arg to 'recollect' to recollect the data, or just use 'load' to
 % use the previously collected data (faster)
+% for recollection, use: collectData( 'recollect', 'crop_data/xlsx_data/' )
 [ elas_data, pq_data, epq_data, cf_data ] = collectData( 'load' );
-epq_data(:,7) = {-0.808};
+%epq_data(:,7) = {-0.808};
 
 %% Update counterfactual data with scenarios
 
@@ -31,7 +32,7 @@ epq_data(:,7) = {-0.808};
 % the cf_data variable is updated here to reflect the percent shocks in 
 % the scenarios_xlsx_file 
 
-crops = {'Corn', 'Soybean', 'Rice'};
+crops = {'Corn', 'Soybean'};
 
 % used when replacing the cf data
 % n = length(cf_data(1,:));
@@ -40,10 +41,13 @@ for scn_crop = crops
 
     % replace with shocks from scenarios_xlsx_file
     [~, ~, scenario_xls_raw]  = xlsread(scenarios_xlsx_file, scn_crop{:});
-
+    
+    % number of scenario years
+    num_years = size(scenario_xls_raw(1,:),2)-2;
+    
     % for each country in cf_data
     for i = 1:size(cf_data, 1)
-
+        
         % counterfactual row's country
         cf_country = cf_data{i, 2};
 
@@ -57,14 +61,14 @@ for scn_crop = crops
 
             if (strcmp(cf_country, scn_country) && ...
                     strcmpi(cf_crop, scn_crop))
-
+                
                 %fprintf('%s: %s\n', cf_country, cf_crop);
                 
                 % given a country-crop match, update the cf_data
                 q_initial = cf_data{i, 4};
                 
                 % percent shocks from scenarios
-                pct_shocks = cell2mat(scenario_xls_raw(j,3:28));
+                pct_shocks = cell2mat(scenario_xls_raw(j,3:(3+num_years-1)));
                 
                 % new quantites implied by scenarios
                 q_scenarios = (1 + pct_shocks) * q_initial;
@@ -85,7 +89,7 @@ for scn_crop = crops
 end
 
 % Drop rows without scenario data 
-ind = ~cellfun(@isempty, cf_data(:,30));
+ind = ~cellfun(@isempty, cf_data(:,size(cf_data,2)));
 cf_data = cf_data(ind, :);
 
 
@@ -117,6 +121,7 @@ try
     writetable(results_table, results_file);
     fprintf('Results saved to %s\n', results_file);
 catch
+    % Make sure current directory is /Climate-Ag-Simulation/
     fprintf('Error saving results\n');
 end
 
